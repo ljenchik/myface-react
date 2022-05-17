749fda7fa10e805f4efb88f1539629222a852d26
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-17 15:18:42 +0100

    add posts to page

--- public/src/src/App.js
+++ public/src/src/App.js
@@ -8,15 +8,17 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:3001/posts")
       .then((response) => response.json())
-      .then((result) => setPosts(result));
+      .then((result) => setPosts(result.results));
   }, []);
 
   return (
     <div className="App">
       <h1>Posts</h1>
-      {posts.map((postData) => (
-        <Post postData={postData} />
-      ))}
+      {posts ? (
+        posts.map((postData, index) => <Post postData={postData} key={index} />)
+      ) : (
+        <div>No Posts </div>
+      )}
     </div>
   );
 }
