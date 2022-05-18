6afd35330b9fd9549fa405f2ad3d37c993ed1d57
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-18 10:50:42 +0100

    add use context

--- public/src/src/App.js
+++ public/src/src/App.js
@@ -1,6 +1,8 @@
 import Post from "./components/posts/Post";
-import { useEffect, useState } from "react";
+import { useEffect, useState, createContext } from "react";
 import "./App.css";
+
+const PostsContext = createContext();
 
 function App() {
   const [posts, setPosts] = useState(null);
@@ -8,17 +10,22 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:3001/posts")
       .then((response) => response.json())
-      .then((result) => setPosts(result.results));
+      .then((result) => setPosts(result.results))
+      .catch((error) => console.log("error"));
   }, []);
 
   return (
     <div className="App">
       <h1>Posts</h1>
-      {posts ? (
-        posts.map((postData, index) => <Post postData={postData} key={index} />)
-      ) : (
-        <div>No Posts </div>
-      )}
+      <PostsContext.Provider value={posts}>
+        {posts ? (
+          posts.map((postData, index) => (
+            <Post postData={postData} key={index} />
+          ))
+        ) : (
+          <div>No Posts </div>
+        )}
+      </PostsContext.Provider>
     </div>
   );
 }
