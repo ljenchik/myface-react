aed15751a6d724a2fd0b9ff2c54824463ff08e74
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-19 14:11:22 +0100

    styling to new post form

--- public/src/src/pages/NewPost.jsx
+++ public/src/src/pages/NewPost.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import "./NewPost.scss";
 
 export default function NewPost() {
-  return <div>New post</div>;
+  return (
+    <div className="NewPost">
+      <h1>New post</h1>
+      <form className="grid-container">
+        <label for="message">Your message:</label>
+        <textarea
+          required
+          name="message"
+          rows="5"
+          cols="30"
+          placeholder="Type your message here..."
+        />
+        <label for="imageUrl">Your image Url:</label>
+        <input
+          className="img-url-box"
+          required
+          type="url"
+          name="imageUrl"
+          placeholder="https://myImage.jpg"
+        />
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
 }
