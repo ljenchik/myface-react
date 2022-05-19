b43d49f2698089cb3ed6fdd95a18e1192e4f04af
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-19 15:47:53 +0100

    add working user create page

--- public/src/src/pages/NewUser.jsx
+++ public/src/src/pages/NewUser.jsx
@@ -11,10 +11,7 @@ export default function NewUser() {
 
   function submitNewUser(event) {
     event.preventDefault();
-
-    if (true) {
-      //do post request
-
+    if (canSubmit()) {
       fetch("http://localhost:3001/users/create/", {
         method: "POST",
         headers: {
@@ -25,73 +22,81 @@ export default function NewUser() {
           username: username,
           email: email,
           coverImageUrl: coverImageUrl,
-          profileImageUrl: profileImageUrl
-        })
+          profileImageUrl: profileImageUrl,
+        }),
+      })
         .then((Response) => console.log(Response))
-        .catch((error) => console.log(error))
-    })
-}
-    function canSubmit() {
-        let nameRegex = /^[a-zA-Z ]+$/
-        let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        let regex = /^https?:\/\/.*\/.*\.(png|webp|jpeg|jpg)\??.*$/gim;
-        return username.length < 20 && username.length > 2 && name.match(nameRegex) && 
-        coverImageUrl.match(regex) && profileImageUrl.match(regex) && email.match(emailRegex);
+        .catch((error) => console.log(error));
     }
+  }
 
+  function canSubmit() {
+    let nameRegex = /^[a-zA-Z ]+$/;
+    let emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regex = /^https?:\/\/.*\/.*\.(png|webp|jpeg|jpg)\??.*$/gim;
     return (
-      <div className="NewUser">
-        <h1>New User</h1>
-
-        <form className="grid-container">
-          <label for="name">Your name:</label>
-          <input
-            required
-            name="name"
-            type="text"
-            placeholder="Type your name..."
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label for="username">Your username:</label>
-          <input
-            required
-            name="username"
-            type="text"
-            placeholder="Type your username..."
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label for="email">Your email:</label>
-          <input
-            required
-            name="email"
-            type="email"
-            placeholder="Type your email..."
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label for="coverImageUrl">Your cover image Url:</label>
-          <input
-            className="img-url-box"
-            required
-            type="url"
-            name="coverImageUrl"
-            placeholder="https://myImage.jpg"
-            onChange={(e) => setCoverImageUrl(e.target.value)}
-          />
-          <label for="profileImageUrl">Your profile image Url:</label>
-          <input
-            className="img-url-box"
-            required
-            type="url"
-            name="profileImageUrl"
-            placeholder="https://myImage.jpg"
-            onChange={(e) => setProfileImageUrl(e.target.value)}
-          />
-
-          <button type="submit" value="Submit" onClick={submitNewUser}>
-            Post
-          </button>
-        </form>
-      </div>
+      username.length < 20 &&
+      username.length > 2 &&
+      name.match(nameRegex) &&
+      coverImageUrl.match(regex) &&
+      profileImageUrl.match(regex) &&
+      email.match(emailRegex)
     );
   }
+
+  return (
+    <div className="NewUser">
+      <h1>New User</h1>
+
+      <form className="grid-container">
+        <label for="name">Your name:</label>
+        <input
+          required
+          name="name"
+          type="text"
+          placeholder="Type your name..."
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label for="username">Your username:</label>
+        <input
+          required
+          name="username"
+          type="text"
+          placeholder="Type your username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label for="email">Your email:</label>
+        <input
+          required
+          name="email"
+          type="email"
+          placeholder="Type your email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label for="coverImageUrl">Your cover image Url:</label>
+        <input
+          className="img-url-box"
+          required
+          type="url"
+          name="coverImageUrl"
+          placeholder="https://myImage.jpg"
+          onChange={(e) => setCoverImageUrl(e.target.value)}
+        />
+        <label for="profileImageUrl">Your profile image Url:</label>
+        <input
+          className="img-url-box"
+          required
+          type="url"
+          name="profileImageUrl"
+          placeholder="https://myImage.jpg"
+          onChange={(e) => setProfileImageUrl(e.target.value)}
+        />
+
+        <button type="submit" value="Submit" onClick={submitNewUser}>
+          Post
+        </button>
+      </form>
+    </div>
+  );
 }
